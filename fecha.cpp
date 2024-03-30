bf11ded373c8dd97f90fc3788eedc2eec4fa3f73
Fernando_Santamaria <fernando.santamaria.leiva@est.una.ac.cr>
2024-03-29 20:09:22 -0600

    Desarrollando la clase fecha.

--- fecha.cpp
+++ fecha.cpp
@@ -6,3 +6,56 @@ fecha::fecha()
 	mes = 0;
 	anio = 0;
 }
+
+fecha::fecha(int d, int m, int a) : dia(d), mes(m), anio(a)
+{
+}
+
+fecha::fecha(fecha& fechaCopia)
+{
+	dia = fechaCopia.getDia();
+	mes = fechaCopia.getMes();  
+	anio = fechaCopia.getAnio(); 
+}
+
+fecha::~fecha()
+{
+}
+
+string fecha::toString()
+{
+	std::stringstream s; 
+	s << "**Fecha del producto**" << endl;
+	s << dia << "/" << mes << "/" << anio << endl;
+	return s.str();
+}
+
+int fecha::getDia()
+{
+	return dia;
+}
+
+int fecha::getMes()
+{
+	return mes;
+}
+
+int fecha::getAnio()
+{
+	return anio;
+}
+
+void fecha::setDia(int d)
+{
+	dia = d;
+}
+
+void fecha::setMes(int m)
+{
+	mes = m;
+}
+
+void fecha::setAnio(int a)
+{
+	anio = a;
+}
--- fecha.h
+++ fecha.h
@@ -3,17 +3,29 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <sstream>
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
 class fecha
 {
-public:
+private:
 	int dia;
 	int mes;
 	int anio;
+public:
 	fecha();
+	fecha(int d, int m, int a);
+	fecha(fecha& fechaCopia);
+	virtual ~fecha();
+	string toString();
+	int getDia();
+	int getMes();
+	int getAnio();
+	void setDia(int d);
+	void setMes(int m);
+	void setAnio(int a);
 };
 
 #endif 
