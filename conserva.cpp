0e74f934b702d55ac0b69f1cdc46a4ceccbd30ce
Fernando_Santamaria <fernando.santamaria.leiva@est.una.ac.cr>
2024-03-31 19:27:55 -0600

    Clase conserva

--- conserva.cpp
+++ conserva.cpp
@@ -1,1 +1,121 @@
 #include "conserva.h"
+
+conserva::conserva(string cod, string nC, string desc, string cat, double pC, int exist, int lim, bool env, fecha* feIng)
+{
+	codigo = cod;
+	nombreComercial = nC;
+	descripcion = desc;
+	categoria = cat;
+	precioCoste = pC;
+	existencia = exist;
+	limite = lim;
+	envasado = env;
+	fechaIngreso = new fecha(*feIng);
+}
+
+conserva::conserva(conserva& conse)
+{
+	codigo = conse.getCodigo();
+	nombreComercial = conse.getNombre();
+	descripcion = conse.getDesc();
+	categoria = conse.getCategoria();
+	precioCoste = conse.getPrecioCosto();
+	existencia = conse.getExistencias();
+	limite = conse.getLimite();
+	envasado = conse.getEnvasado();
+	fechaIngreso = conse.getFechaIngreso();
+}
+
+conserva::~conserva()
+{
+}
+
+string conserva::getCodigo()
+{
+	return codigo;
+}
+
+string conserva::getNombre()
+{
+	return nombreComercial;
+}
+
+string conserva::getDesc()
+{
+	return descripcion;
+}
+
+string conserva::getCategoria()
+{
+	return categoria;
+}
+
+int conserva::getExistencias()
+{
+	return existencia;
+}
+
+int conserva::getLimite()
+{
+	return limite;
+}
+
+double conserva::getPrecioCosto()
+{
+	return precioCoste;
+}
+
+bool conserva::getEnvasado()
+{
+	return envasado;
+}
+
+fecha* conserva::getFechaIngreso()
+{
+	return fechaIngreso;
+}
+
+void conserva::setCodigo(string cod)
+{
+	codigo = cod;
+}
+
+void conserva::setNombre(string nomComercial)
+{
+	nombreComercial = nomComercial
+}
+
+void conserva::setDescripcion(string desc)
+{
+	descripcion = desc;
+}
+
+void conserva::setCategoria(string cat)
+{
+	categoria = cat;
+}
+
+void conserva::setExistencias(int exist)
+{
+	existencia = exist;
+}
+
+void conserva::setLimite(int lim)
+{
+	limite = lim;
+}
+
+void conserva::setPrecioCosto(double preCosto)
+{
+	precioCoste = preCosto;
+}
+
+void conserva::setEnvasado(bool env)
+{
+	envasado = env;
+}
+
+string conserva::toString()
+{
+	return string();
+}
--- conserva.h
+++ conserva.h
@@ -4,6 +4,42 @@
 class conserva : protected producto
 {
 private:
+	string codigo;
+	string nombreComercial;
+	string descripcion;
+	string categoria;
+	double precioCoste;
+	int existencia;
+	int limite;
 	bool envasado;
+	fecha* fechaIngreso;
+public:
+	conserva(string, string, string, string, double, int, int, bool, fecha*);
+	conserva(conserva&);
+	virtual ~conserva();
+
+
+	//Get's
+	string getCodigo();
+	string getNombre();
+	string getDesc();
+	string getCategoria();
+	int getExistencias();
+	int getLimite();
+	double getPrecioCosto();
+	bool getEnvasado();
+	fecha* getFechaIngreso();
+
+	//Set's
+	void setCodigo(string);
+	void setNombre(string);
+	void setDescripcion(string);
+	void setCategoria(string);
+	void setExistencias(int);
+	void setLimite(int);
+	void setPrecioCosto(double);
+	void setEnvasado(bool);
+
+	string toString();
 };
 #endif
