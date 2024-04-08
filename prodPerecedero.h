ff604ea876338e1e38cd413a0416e2c4347e8c07
Fernando_Santamaria <fernando.santamaria.leiva@est.una.ac.cr>
2024-04-07 21:40:08 -0600

    Cambios en la clase proPerecedero

--- prodPerecedero.h
+++ prodPerecedero.h
@@ -5,23 +5,36 @@
 class prodPerecedero : public perecedero, public producto
 {
 public:
-    prodPerecedero();
-    virtual string getCodigo() override;
-    virtual string getNombre() override;
-    virtual string getDesc() override;
-    virtual string getCategoria() override;
-    virtual double getPrecioCosto() override;
-    virtual int getExistencias() override;
-    virtual int getLimite() override;
+    virtual string getCodigo() = 0;
+    virtual string getNombre() = 0;
+    virtual string getDesc() = 0;
+    virtual string getCategoria() = 0;
+    virtual double getPrecioCosto() = 0;
+    virtual int getExistencias() = 0;
+    virtual int getLimite() = 0;
+    virtual bool getNacional() = 0;
+    virtual double getPeso() = 0;
 
-    virtual string toString() override;
+    virtual string toString() = 0;
 
-    virtual void setCodigo(string) override;
-    virtual void setNombre(string) override;
-    virtual void setDescripcion(string) override;
-    virtual void setCategoria(string) override;
-    virtual void setExistencias(int) override;
-    virtual void setPrecioCosto(double) override;
-    virtual void setLimite(int) override;
+    virtual void setCodigo(string) = 0;
+    virtual void setNombre(string) = 0;
+    virtual void setDescripcion(string) = 0;
+    virtual void setCategoria(string) = 0;
+    virtual void setExistencias(int) = 0;
+    virtual void setPrecioCosto(double) = 0;
+    virtual void setLimite(int) = 0;
+    virtual void setNacional(bool) = 0;
+    virtual void setPeso(double) = 0;
+protected:
+    string codigo;
+    string nombreComercial;
+    string descripcion;
+    double precioCosto;
+    string categoria;
+    int existencias;
+    int limite;
+    bool nacional;
+    double peso;
 };
 #endif
