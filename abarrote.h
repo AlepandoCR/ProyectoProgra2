c38f1d40d5a5ce2b0e7feb5acf0c3ebc95fd93d3
Fernando_Santamaria <fernando.santamaria.leiva@est.una.ac.cr>
2024-04-15 20:27:49 -0600

    cambios abarrote

--- abarrote.h
+++ abarrote.h
@@ -3,8 +3,45 @@
 #include "prodPerecedero.h"
 class abarrote : public prodPerecedero
 {
+public:
+    abarrote();
+    abarrote(string, string, string, string, string, double, int, int, bool, double, fecha*);
+    ~abarrote();
+
+    string getCodigo();
+    string getNombre();
+    string getDesc();
+    string getCategoria();
+    string getNombreEmpresa();
+    double getPrecioCosto();
+    int getExistencias();
+    int getLimite();
+    bool getNacional();
+    double getPeso();
+
+    string toString();
+
+    void setCodigo(string);
+    void setNombre(string);
+    void setDescripcion(string);
+    void setCategoria(string);
+    void setNombreEmpresa(string);
+    void setExistencias(int);
+    void setPrecioCosto(double);
+    void setLimite(int);
+    void setNacional(bool);
+    void setPeso(double);
 private:
-	string nombreEmpresa;
+    string codigo;
+    string nombreComercial;
+    string descripcion;
+    double precioCosto;
+    string categoria;
+    int existencias;
+    int limite;
+    bool nacional;
+    double peso;
+    string nombreEmpresa;
 };
 
 #endif
