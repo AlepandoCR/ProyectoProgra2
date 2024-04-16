7fe4e779c59da67271d3ed08da1623da3e7d955a
Fernando_Santamaria <fernando.santamaria.leiva@est.una.ac.cr>
2024-04-15 20:01:44 -0600

    Cambios perecedero

--- perecedero.h
+++ perecedero.h
@@ -18,6 +18,28 @@ public:
     perecedero();
     perecedero(string, string, string, string, double, int, int, bool, double, fecha*);
     ~perecedero();
+
+    virtual string getCodigo();
+    virtual string getNombre();
+    virtual string getDesc();
+    virtual string getCategoria();
+    virtual double getPrecioCosto();
+    virtual int getExistencias();
+    virtual int getLimite();
+    virtual bool getNacional();
+    virtual double getPeso();
+
+    virtual string toString();
+
+    virtual void setCodigo(string);
+    virtual void setNombre(string);
+    virtual void setDescripcion(string);
+    virtual void setCategoria(string);
+    virtual void setExistencias(int);
+    virtual void setPrecioCosto(double);
+    virtual void setLimite(int);
+    virtual void setNacional(bool);
+    virtual void setPeso(double);
 };
 
 #endif
