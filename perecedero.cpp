65d3e696ee73a6e6115f16b5f384fff1a53d9906
Fernando_Santamaria <fernando.santamaria.leiva@est.una.ac.cr>
2024-04-15 20:03:32 -0600

    Cambios perecedero

--- perecedero.cpp
+++ perecedero.cpp
@@ -21,3 +21,98 @@ perecedero::perecedero(string cod, string nC, string desc, string cat, double pC
 perecedero::~perecedero()
 {
 }
+
+string perecedero::getCodigo()
+{
+	return codigo;
+}
+
+string perecedero::getNombre()
+{
+	return nombreComercial;
+}
+
+string perecedero::getDesc()
+{
+	return descripcion;
+}
+
+string perecedero::getCategoria()
+{
+	return categoria;
+}
+
+double perecedero::getPrecioCosto()
+{
+	return precioCosto;
+}
+
+int perecedero::getExistencias()
+{
+	return existencias;
+}
+
+int perecedero::getLimite()
+{
+	return limite;
+}
+
+bool perecedero::getNacional()
+{
+	return nacional;
+}
+
+double perecedero::getPeso()
+{
+	return peso;
+}
+
+string perecedero::toString()
+{
+	return string();
+}
+
+void perecedero::setCodigo(string cod)
+{
+	codigo = cod;
+}
+
+void perecedero::setNombre(string nC)
+{
+	nombreComercial = nC;
+}
+
+void perecedero::setDescripcion(string desc)
+{
+	descripcion = desc;
+}
+
+void perecedero::setCategoria(string cat)
+{
+	categoria = cat;
+}
+
+void perecedero::setExistencias(int exis)
+{
+	existencias = exis;
+}
+
+void perecedero::setPrecioCosto(double pC)
+{
+	precioCosto = pC;
+}
+
+void perecedero::setLimite(int lim)
+{
+	limite = lim;
+}
+
+void perecedero::setNacional(bool nac)
+{
+	nacional = nac;
+}
+
+void perecedero::setPeso(double pes)
+{
+	peso = pes;
+}
